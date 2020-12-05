18c6d3616cde0d9d5c938983213a09139f1a43db
Ollie Blair <ollieblair@MacBook-Pro.local>
2020-12-05 08:55:55 +0000

    Initial checkin

---
+++ Bond Pricing Application/src/main/java/com/oblair/nea/bond/BondApplication.java
@@ -0,0 +1,28 @@
+package com.oblair.nea.bond;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		BondApplication.class,
+		Jsr310JpaConverters.class 
+})
+public class BondApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BondApplication.class, args);
+	}
+
+}
